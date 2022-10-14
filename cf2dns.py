c736ecd9bb29d6deb7dfb15f625bf4433e648365
lengxi <62014207+lenxiys@users.noreply.github.com>
2022-10-14 21:04:26 +0800

    Update cf2dns.py

--- cf2dns.py
+++ cf2dns.py
@@ -16,20 +16,20 @@ from log import Logger
 import traceback
 
 #可以从https://shop.hostmonit.com获取
-KEY = "o1zrmHAF"
+KEY = "YIZQ8iYhGXbC"
 
 #CM:移动 CU:联通 CT:电信  AB:境外 DEF:默认
 #修改需要更改的dnspod域名和子域名
 DOMAINS = {
-    "hostmonit.com": {"@": ["CM","CU","CT"], "shop": ["CM", "CU", "CT"], "stock": ["CM","CU","CT"]},
-    "xxxx.me": {"@": ["CM","CU","CT"], "vv": ["CM","CU","CT"]}
+    "52xnet.cn": {"@": ["CM","CU","CT"], "shop": ["CM", "CU", "CT"], "stock": ["CM","CU","CT"]},
+    "www.52xnet.cn": {"@": ["CM","CU","CT"], "vv": ["CM","CU","CT"]}
 }
 
 #解析生效条数 免费的DNSPod相同线路最多支持2条解析
 AFFECT_NUM = 2
 
 #DNS服务商 如果使用DNSPod改为1 如果使用阿里云解析改成2
-DNS_SERVER = 1
+DNS_SERVER = 2
 
 #解析生效时间，默认为600秒 如果不是DNS付费版用户 不要修改!!!
 TTL = 600
@@ -40,8 +40,8 @@ TYPE = 'v4'
 #API 密钥
 #腾讯云后台获取 https://console.cloud.tencent.com/cam/capi
 #阿里云后台获取 https://help.aliyun.com/document_detail/53045.html?spm=a2c4g.11186623.2.11.2c6a2fbdh13O53  注意需要添加DNS控制权限 AliyunDNSFullAccess
-SECRETID = 'AKIDVmxtxxxxxxxxxxxfo8CzfjgN'
-SECRETKEY = 'ZrVszqxxxxxxxxxxjOp1zVl'
+SECRETID = 'LTAI5tAbGUs2Ku4uXKsdhd2v'
+SECRETKEY = 'ozckqAN4cluXU69ZFT5WxIAC7tkpgB'
 
 log_cf2dns = Logger('cf2dns.log', level='debug') 
 urllib3.disable_warnings()

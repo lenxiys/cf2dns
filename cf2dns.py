f939d1584f1e652a721c4d5e565a5d490cb440b1
lengxi <62014207+lenxiys@users.noreply.github.com>
2022-10-14 21:43:14 +0800

    Update cf2dns.py

--- cf2dns.py
+++ cf2dns.py
@@ -21,8 +21,7 @@ KEY = "YIZQ8iYhGXbCfgfw"
 #CM:移动 CU:联通 CT:电信  AB:境外 DEF:默认
 #修改需要更改的dnspod域名和子域名
 DOMAINS = {
-    "52xnet.cn": {"@": ["CM","CU","CT"], "www": ["CM", "CU", "CT"]},
-    "": {"@": ["CM","CU","CT"], "www": ["CM","CU","CT"]}
+    "52xnet.cn": {"@": ["CM","CU","CT"], "www": ["CM", "CU", "CT"]}
 }
 
 #解析生效条数 免费的DNSPod相同线路最多支持2条解析
